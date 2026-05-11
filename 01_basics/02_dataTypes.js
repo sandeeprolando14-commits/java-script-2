8faa1ef5b99de861b1d2512895e197d5480b7a18
sandeeprolando14-commits <sandeeprolando14@gmail.com>
2026-05-11 17:10:47 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code as newer version
+
+console.log(3+3) // code readability should be high
+console.log("sandeep")
+
+
+let name = "sandepp"
+let age = 20
+let isloggedin = false
+let state;
+
+// listed below are premitive data type
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null (stand alone value)
+// undefined =>
+// symbol => unique
+// listed below are premitive data type
+
+
+
+// object
+
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined)
